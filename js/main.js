5e6a97b120e1dad60ccc671e9e1ee8e74c99cd82
Letov Sergey <letovs87@gmail.com>
2024-11-12 15:33:33 +0300

    finish

--- js/main.js
+++ js/main.js
@@ -18,7 +18,7 @@ const arr2 =  [
   { name: 'otter', likes: 'fish biscuits' }
 ];
 
-var res2 = concat(arr2);
+const res2 = concat(arr2);
 console.log(res2);
 function concat(arr2){
     return arr2.map(item => item.name + ' likes ' + item.likes);
@@ -54,4 +54,110 @@ function getOdd(arr) {
         }
         return result;
     }, []);
+}
+
+// Задание 6 (оно же 2)
+const shop = [
+    { price: 10, count: 2},
+    { price: 100, count: 1},
+    { price: 2, count: 5},
+    { price: 15, count: 6},
+]
+
+const result = shop.reduce((result, item) => {
+    return result + item.price * item.count;
+}, 0)
+
+console.log(result);
+
+// Задание 7
+const seq = [1, 2, 2, 4, 5, 5];
+
+const unique = getUniqueValues(seq);
+
+console.log(unique);
+
+function getUniqueValues(seq) {
+    return seq.reduce((unique, item) => {
+        if (unique.indexOf(item) === -1) {
+            unique.push(item);
+        }
+        return unique;
+    }, []);
+}
+
+// Задание 8
+const exceptionsCode = [
+    500, 401, 402, 403, 404
+]
+
+exceptionsCode.forEach((item) => console.log(getErrorMessage(item)));
+
+function getErrorMessage(exceptionCode){
+    switch (exceptionCode) {
+        case 404:
+            return 'Не найдено';
+        case 403:
+            return 'Доступ запрещен';
+        case 402:
+            return 'Ошибка сервера';
+        case 401:
+            return 'Ошибка авторизации';
+        case 500:
+            return 'Ошибка сервера';
+    }
+}
+
+// Задание 9
+// Напишите функцию, которая возвращает 2 наименьших значение массива
+const seqMin = [4,3,2,1];
+const twoMin = getSeqMin(seqMin);
+console.log(twoMin);
+function getSeqMin(seq) {
+    const sortedArr = seq.sort((a, b) => a - b);
+    return sortedArr.slice(0, 2);
+}
+
+
+// Задание 10
+const person = {
+   firstName: 'Петр',
+   secondName: 'Васильев',
+   patronymic: 'Иванович'
+}
+
+const fullName = getFullName(person);
+console.log(fullName);
+function getFullName(person) {
+    return `ФИО: ${person.firstName} ${person.patronymic} ${person.secondName}`;
+}
+
+// Задание 11
+const multyArr = [1,2,3,4];
+const value = 5;
+
+const newArr = multyArrs(multyArr, value)
+console.log(newArr);
+function multyArrs(multyArr, value) {
+    return multyArr.map((item) => {
+        return item * value;
+    })
+}
+
+// Задание 12
+const heroes =  [
+   {name: 'Batman', franchise: 'DC'},
+   {name: 'Ironman', franchise: 'Marvel'},
+   {name: 'Thor', franchise: 'Marvel'},
+   {name: 'Superman', franchise: 'DC'}
+]
+const franchise = 'Marvel';
+const franchiseHeroes = getFranchiseHeroes(heroes, franchise);
+console.log(franchiseHeroes);
+
+function getFranchiseHeroes(heroes, franchise) {
+    return heroes
+        .filter(x => x.franchise.includes(franchise))
+        .map(hero => hero.name)
+        .join(', ');
 }
